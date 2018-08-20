8be97d1017a07e35c2ee6335b4f55eada5707c3e
sp4rk2 <sp4rk2sp4rk2@gmail.com>
2018-08-20 23:41:11 +0100

    abc

---
+++ Power2.lua
@@ -0,0 +1,31 @@
+local component = require("component")
+
+local  gpu = component.gpu
+
+local width, height = gpu.getResolution()
+
+local frameColour = 0x333333
+
+function clearScreen()
+	local oldColour = gpu.getBackground(false)
+	gpu.setBackground(0x000000, false)
+	gpu.fill(1, 1, w, h, " ")
+	gpu.setBackground(oldColour, false)
+end
+
+function frame()
+	local oldColour = gpu.getBackground(false)
+	gpu.setBackground(frameColour, false)
+	gpu.fill(1, 1, w, 1, " ")
+	gpu.setBackground(oldColour, false)
+end
+
+function main()
+	clearScreen()
+	frame()
+end
+
+while true do
+	main()
+	os.sleep(0.25)
+end
