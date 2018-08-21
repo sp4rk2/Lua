72a0f1eccdab1b2d0496f446998e32e05e7084cf
sp4rk2 <sp4rk2sp4rk2@gmail.com>
2018-08-21 14:51:22 +0100

    abc

--- power3.lua
+++ power3.lua
@@ -11,7 +11,7 @@ local barColour2 = 0x34E2E2
 
 local header = {
 	"Mainframe Power Monitor", 
-	"(MPMv0.2)"
+	"(MPMv0.1)"
 }
 
 local headerColour = 0xFFFFFF
@@ -33,18 +33,6 @@ local highestRF = component.energy_device.getEnergyStored() * cellCount
 local highestRFTime = 0
 
 local colourSwitch = true
-
-local headerSpace = 0
-local headerPaddingLeft = 0
-
-local statisticsPaddingLeft = 0
-local statisticsPaddingTop = 0
-local titleSpace = 0
-
-local graphLength = 0
-
-local graphPaddingLeft = 0
-local graphPaddingTop = 0
 
 function clearScreen()
 	local oldColour = gpu.getBackground(false)
@@ -88,13 +76,14 @@ function tableLength(tableInput)
   return output
 end
 
-
-
 function setHeader()
 	local oldColour = gpu.getForeground(false)
 	gpu.setForeground(headerColour, false)
+
+	local headerSpace = (width / 4) - 3
+	local paddingLeft = ((width / 4) * 3) + 1
 	for index=1, tableLength(header) do
-		gpu.set(headerPaddingLeft, index + 3, centerText(header[index], headerSpace))
+		gpu.set(paddingLeft, index + 3, centerText(header[index], headerSpace))
 	end
 	gpu.setForeground(oldColour, false)
 end
@@ -103,41 +92,37 @@ function setStatistics()
 	local oldColour = gpu.getForeground(false)
 	gpu.setForeground(titleColour, false)
 
-	gpu.set(statisticsPaddingLeft - 1, statisticsPaddingTop, centerText("Statistics", titleSpace))
-	statisticsPaddingTop = statisticsPaddingTop + 2
+	local paddingLeft = ((width / 4) * 3) + 2
+	local paddingTop = 8 + tableLength(header)
+	local titleSpace = (width / 4) - 3
+
+	gpu.set(paddingLeft - 1, paddingTop, centerText("Statistics", titleSpace))
+	paddingTop = paddingTop + 2
 
 	gpu.setForeground(statisticKeyColour, false)
-	gpu.set(statisticsPaddingLeft, statisticsPaddingTop, "Cell Count: ")
-	gpu.set(statisticsPaddingLeft, statisticsPaddingTop + 1, "TSLU: ")
-	gpu.set(statisticsPaddingLeft, statisticsPaddingTop + 3, "Available: ")
-	gpu.set(statisticsPaddingLeft, statisticsPaddingTop + 5, "Lowest: ")
-	gpu.set(statisticsPaddingLeft, statisticsPaddingTop + 6, "Lowest Time: ")
-	gpu.set(statisticsPaddingLeft, statisticsPaddingTop + 8, "Highest: ")
-	gpu.set(statisticsPaddingLeft, statisticsPaddingTop + 9, "Highest Time: ")
+	gpu.set(paddingLeft, paddingTop, "Cell Count: ")
+	gpu.set(paddingLeft, paddingTop + 1, "TSLU: ")
+	gpu.set(paddingLeft, paddingTop + 3, "Available: ")
+	gpu.set(paddingLeft, paddingTop + 5, "Lowest: ")
+	gpu.set(paddingLeft, paddingTop + 6, "Lowest Time: ")
+	gpu.set(paddingLeft, paddingTop + 8, "Highest: ")
+	gpu.set(paddingLeft, paddingTop + 9, "Highest Time: ")
 
 	gpu.setForeground(statisticValueColour, false)
-	gpu.set(width - string.len(tostring(cellCount)) - 3, statisticsPaddingTop, tostring(cellCount))
-	gpu.set(width - string.len(tostring(updateTime) .. "seconds") - 3, statisticsPaddingTop + 1, tostring(updateTime) .. "seconds")
-	gpu.set(width - string.len(tostring(available) .. "/" .. tostring(max)) - 3, statisticsPaddingTop + 3, tostring(available) .. "/" .. tostring(max))
-	gpu.set(width - string.len(tostring(lowestRF) .. "RF") - 3, statisticsPaddingTop + 5, tostring(lowestRF) .. "RF")
-	gpu.set(width - string.len(tostring(lowestRFTime)) - 3, statisticsPaddingTop + 6, tostring(lowestRFTime))
-	gpu.set(width - string.len(tostring(highestRF) .. "RF") - 3, statisticsPaddingTop + 8, tostring(highestRF) .. "RF")
-	gpu.set(width - string.len(tostring(highestRFTime)) - 3, statisticsPaddingTop + 9, tostring(highestRFTime))
+	gpu.set(width - string.len(tostring(cellCount)) - 3, paddingTop, tostring(cellCount))
+	gpu.set(width - string.len(tostring(updateTime) .. "seconds") - 3, paddingTop + 1, tostring(updateTime) .. "seconds")
+	gpu.set(width - string.len(tostring(available) .. "/" .. tostring(max)) - 3, paddingTop + 3, tostring(available) .. "/" .. tostring(max))
+	gpu.set(width - string.len(tostring(lowestRF) .. "RF") - 3, paddingTop + 5, tostring(lowestRF) .. "RF")
+	gpu.set(width - string.len(tostring(lowestRFTime)) - 3, paddingTop + 6, tostring(lowestRFTime))
+	gpu.set(width - string.len(tostring(highestRF) .. "RF") - 3, paddingTop + 8, tostring(highestRF) .. "RF")
+	gpu.set(width - string.len(tostring(highestRFTime)) - 3, paddingTop + 9, tostring(highestRFTime))
+
 	gpu.setForeground(oldColour, false)
+
 end
 
-function calculateValues()
-	headerSpace = (width / 4) - 3
-	headerPaddingLeft = ((width / 4) * 3) + 1
-
-	statisticsPaddingLeft = ((width / 4) * 3) + 2
-	statisticsPaddingTop = 8 + tableLength(header)
-	titleSpace = (width / 4) - 3
-
-	graphLength = (width / 4 * 3) - 11
-
-	graphPaddingLeft = (width / 4 * 3) - 2
-	graphPaddingTop = height - 1
+function setRFValues()
+	local graphLength = (width / 4 * 3) - 11
 	for index = 1, graphLength do
 		valuesRF[index] = 1
 	end
@@ -147,10 +132,10 @@ function updateRFValues()
 	available = component.energy_device.getEnergyStored() * cellCount
 
 	if available >= highestRF then
-		highestRF = available
+		highestRF = component.energy_device.getEnergyStored() * cellCount
 	end
 	if available <= lowestRF then
-		lowestRF = available
+		lowestRF = component.energy_device.getEnergyStored() * cellCount
 	end
 	percent = round((available / max) * height - 20, 0)
 
@@ -163,7 +148,10 @@ end
 -- end
 
 function graph()
+	local oldColour = gpu.getBackground(false)
 	gpu.setBackground(0xFFFFFF, false)
+	local paddingLeft = (width / 4 * 3) - 2
+	local paddingTop = height - 1
 	for index = 1, tableLength(valuesRF) do
 		if colourSwitch == true then
 			gpu.setBackground(barColour1, false)
@@ -173,14 +161,19 @@ function graph()
 			colourSwitch = true
 		end
 
-		gpu.fill(graphPaddingLeft, graphPaddingTop - valuesRF[tableLength(valuesRF) - index + 1], 1, valuesRF[tableLength(valuesRF) - index + 1], " ")
+		gpu.fill(paddingLeft, paddingTop - valuesRF[tableLength(valuesRF) - index + 1], 1, valuesRF[tableLength(valuesRF) - index + 1], " ")
 
-		graphPaddingLeft = graphPaddingLeft - 1
+		paddingLeft = paddingLeft - 1
+
 	end
+
+	gpu.setBackground(oldColour, false)
+
+
 end
 
 function main()
-	calculateValues()
+	setRFValues()
 	while true do
 		updateRFValues()
 		clearScreen()
